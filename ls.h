09982679da0151910b1627b3131ce4ca7f6ff20b
regening <2791186095@qq.com>
2026-01-18 00:43:19 +0800

    linux目录操作

---
+++ ls.h
@@ -0,0 +1,15 @@
+#ifndef LS_H
+#define LS_H
+
+#include <dirent.h> 
+#include <sys/stat.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void sort_and_list(const char *path, int show_long, int show_hidden, int show_recursive, int reverse, int show_inode, int show_size, int show_time);
+void list_directory(const char *path, int show_long, int show_hidden, int show_recursive, int show_inode, int show_size, int sort_by_time, int reverse, int show_time);
+void print_long_format(struct dirent *entry, const char *path, int show_size, int show_inode);
+
+#endif
+
