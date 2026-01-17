505f3d4e759622405d881bcf7c811e6b8461349e
regening <2791186095@qq.com>
2026-01-18 00:42:12 +0800

    linux目录操作

--- main.c
+++ main.c
@@ -1,49 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
-#include "ls.c"  // 引入ls.c中的函数
-
-void sort_and_list(const char *path, int show_long, int show_hidden, int show_recursive, int reverse, int show_inode, int show_size);
+#include "ls.h"  // 引入ls.h中的函数声明
 
 int main(int argc, char *argv[]) {
-    int list_a = 0;
-    int list_l = 0;
-    int list_R = 0;
-    int list_r = 0;
-    int list_i = 0;
-    int list_s = 0;
-    char *path = ".";  // 默认路径是当前目录
+    int show_long = 0;
+    int show_hidden = 0;
+    int show_recursive = 0;
+    int reverse = 0;
+    int show_inode = 0;
+    int show_size = 0;  
+    int show_time = 0;    // 判断是否按时间排序
+    char *path = ".";  // 默认 是当前目录
 
     int option;
     while ((option = getopt(argc, argv, "alRtrsi")) != -1) {
         switch (option) {
-
-            
-            case 'a':
-                list_a = 1;
+            case 'a':   //实现-a
+                show_hidden = 1;
                 break;
-            case 'l':
-                list_l = 1;
+            case 'l':   //实现-l
+                show_long = 1;
                 break;
-            case 'R':
-                list_R = 1;
+            case 'R':   //实现-R
+                show_recursive = 1;
                 break;
-            case 'r':
-                list_r = 1;
+            case 't':   //实现-t
+                show_time = 1;
                 break;
-            case 'i':
-                list_i = 1;
+            case 'r':   //实现-r
+                reverse = 1;
                 break;
-            case 's':
-                list_s = 1;
+            case 'i':   //实现-i
+                show_inode = 1;
                 break;
-            case 't':   // 可以添加排序功能
+            case 's':   //实现-s
+                show_size = 1;
                 break;
             default:
-                fprintf(stderr, "Usage: %s [-a] [-l] [-R] [-r] [-i] [-s] [-t] [path]\n", argv[0]);
+                fprintf(stderr, "Usage: %s [-a] [-l] [-R] [-r] [-t] [-s] [-i] [path]\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
+
+    if (optind < argc) {    // 如果指定了路径，使用它
+        path = argv[optind];
+    }
+
+    sort_and_list(path, show_long, show_hidden, show_recursive, reverse, show_inode, show_size, show_time);  // 按照指定参数列出目录内容
 
     return 0;
 }
